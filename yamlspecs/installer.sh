dffefda6aaafd211d064ba29555198181db528aa
Philip Papadopoulos <ppapadop@uci.edu>
2021-07-22 14:40:51 -0700

    Cuda driver and installer updates for Cuda11. Filter nsight rpm requires. remove cuda-driver from manifest (handled separately)

--- yamlspecs/installer.sh
+++ yamlspecs/installer.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 # Install the NVIDIA driver with the run file
 
+VERSION=%VERSION%
 THISDIR=$(dirname $(realpath $0))
 RUNFILE=$(ls $THISDIR/NVIDIA-Linux-x86_64*run)
-NVIDIA_INSTALLER=$(which nvidia-installer 2>/dev/null)
 NVIDIA_SMI=/usr/bin/nvidia-smi
-
+INSTALLED_VERSION=/etc/nvidia-driver-version
 INSTALL_ARGS="--silent  --no-nouveau-check  --no-install-libglvnd"
 
 NOUVEAU=1
+
+uninstall_if_old() {
+# unistall the driver if the file $INSTALLED_VERSION is incorrect 
+# or file does not exist
+        if [ -f $INSTALLED_VERSION ] && [ $(cat $INSTALLED_VERSION) == "$VERSION" ]; then return; fi
+        $RUNFILE --silent --uninstall
+}
 
 nouveau_check () {
 # Set NOUVEAU to 0 if the module is not loaded 
@@ -31,10 +38,15 @@ if [ $NOUVEAU == 1 ]; then
         fi
 fi
 
+uninstall_if_old
+
+NVIDIA_INSTALLER=$(which nvidia-installer 2>/dev/null)
+
 if [ "$NVIDIA_INSTALLER" == "" ]; then
    echo "NVIDIA not installed ... installing "
    chmod +x $RUNFILE
    $RUNFILE $INSTALL_ARGS 2>&1 | tee /var/log/nvidia-installer-run.log
+   echo -n $VERSION > $INSTALLED_VERSION 
 fi
 
 # Create /dev/nvidia* by running nvidia-smi, if it exists
