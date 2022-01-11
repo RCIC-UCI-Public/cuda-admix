ce8e72084e73272f55561c49da30c08a47138569
Philip M. Papadopoulos <ppapadop@uci.edu>
2022-01-11 11:53:38 -0800

    Small tweaks for building on RHEL. need to turn off build id links for cuda-samples. need to unset LDFLAGS before Make in cuda[11]-samples. updated nsight filter requires to take out generic libGL.so()(64bit) since nothing provides it.

--- yamlspecs/filter-requires-cuda-toolkit-nsight.sh
+++ yamlspecs/filter-requires-cuda-toolkit-nsight.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #
 # remove requirement for osgi 
-/usr/lib/rpm/find-requires $* | sed -e '/osgi\.*/d' | sed -e '/^[[:space:]]*$/d' | sed -e '/^#/d' |sed -e '/GLIBC/d' | sed -e '/aarch/d' | sed -e '/ld64.so.2/d'
+/usr/lib/rpm/find-requires $* | sed -e '/osgi\.*/d' | sed -e '/^[[:space:]]*$/d' | sed -e '/^#/d' |sed -e '/GLIBC/d' | sed -e '/aarch/d' | sed -e '/ld64.so.2/d' | sed '/libGL.so()(64bit)/d'
