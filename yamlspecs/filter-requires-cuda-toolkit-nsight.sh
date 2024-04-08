7f646d2397a1bb12ac7919e755a06d4195f6a155
Nadya Williams <nadya.williams@gmail.com>
2024-04-08 13:56:42 -0700

    need to add libQt5* libraries for cuda 10. They are provioded by the RPM itself. Previously  some nsight subdirs were not installed so this did not show up till the restructureing per the previous commit

--- yamlspecs/filter-requires-cuda-toolkit-nsight.sh
+++ yamlspecs/filter-requires-cuda-toolkit-nsight.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #
 # remove requirement for osgi 
-/usr/lib/rpm/find-requires $* | sed -e '/libQt6\.*/d' | sed -e '/osgi\.*/d' | sed -e '/^[[:space:]]*$/d' | sed -e '/^#/d' |sed -e '/GLIBC/d' | sed -e '/aarch/d' | sed -e '/ld64.so.2/d' | sed '/libGL.so()(64bit)/d'
+/usr/lib/rpm/find-requires $* | sed -e '/libQt5\.*/d' | sed -e '/libQt6\.*/d' | sed -e '/osgi\.*/d' | sed -e '/^[[:space:]]*$/d' | sed -e '/^#/d' |sed -e '/GLIBC/d' | sed -e '/aarch/d' | sed -e '/ld64.so.2/d' | sed '/libGL.so()(64bit)/d'
