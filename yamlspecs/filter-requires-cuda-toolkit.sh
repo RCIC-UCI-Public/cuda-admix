8d4adba694f9a9a3d3fe4708ea7b798ee74e2156
Nadya Williams <nadya.williams@gmail.com>
2026-01-13 16:57:35 -0800

    remove libpytohn dependencies. They are  not real but prevent installing RPM.

--- yamlspecs/filter-requires-cuda-toolkit.sh
+++ yamlspecs/filter-requires-cuda-toolkit.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #
 # remove requirement that htslib depends on libhts.so
-/usr/lib/rpm/find-requires $* | sed -e '/osgi\.*/d' | sed -e '/^[[:space:]]*$/d' | sed -e '/^#/d'
+/usr/lib/rpm/find-requires $* | sed -e '/osgi\.*/d' | sed -e '/^[[:space:]]*$/d' | sed -e '/^#/d' | sed -e '/libpython\.*/d'
